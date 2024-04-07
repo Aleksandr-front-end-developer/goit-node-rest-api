dff5abc4200e0b26ca20ef963d3aa3a2f64ac53d
Oleksandr Danyliuk <oleksandr.danyliuk.88@gmail.com>
2024-04-07 13:28:00 +0300

    fix error

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -94,7 +94,7 @@ const verificationRequest = async (req, res) => {
   const { verificationToken } = req.params;
   const user = await User.findOne({ verificationToken });
   if (!user) {
-    throw Http(404, "User not found");
+    throw HttpError(404, "User not found");
   }
   await User.findByIdAndUpdate(user._id, {
     verify: true,
